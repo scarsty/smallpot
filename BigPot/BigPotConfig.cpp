38e546c0921bd57049a73b13aafa843a37ec0391
cd <cd@2707ce61-8f64-3b48-aa73-004342374819>
2015-03-14 04:53:12 +0000

    git-svn-id: https://svn.mythkast.net/svn/bigpot@39 2707ce61-8f64-3b48-aa73-004342374819

--- BigPot/BigPotConfig.cpp
+++ BigPot/BigPotConfig.cpp
@@ -14,12 +14,23 @@ void BigPotConfig::init()
 {
 	_filename = filepath_ + "/config.json";
 	printf("try find config file: %s\n", _filename.c_str());
+
+	
 	Json::Reader reader;
 	_content = readStringFromFile(_filename);
 	reader.parse(_content, _value);
 
 	if (_value["record"].isObject())
 		_record = _value["record"];
+
+	_doc.LoadFile(_filename.c_str());
+
+	//_doc.FirstChildElement("volume");
+	if (_doc.Error())
+		_doc.Clear();
+	XMLDeclaration *pDel = _doc.NewDeclaration("xml version=\"1.0\" encoding=\"UTF-8\"");
+	_doc.NewElement("volume");
+	//_doc.
 }
 
 void BigPotConfig::write()
@@ -29,8 +40,10 @@ void BigPotConfig::write()
 	_value["record"] = _record;
 	_content = writer.write(_value);
 
-	ofstream ofs;
-	ofs.open(_filename);
-	ofs << _content;
+	//ofstream ofs;
+	//ofs.open(_filename);
+	//ofs << _content;
+
+	auto x=_doc.SaveFile(_filename.c_str());
 }
 
