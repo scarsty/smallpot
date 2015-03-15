f16c571988742210169cad35ca148869b1126ad4
mythkast <mythkast@2707ce61-8f64-3b48-aa73-004342374819>
2015-03-15 11:55:25 +0000

    dddd
    git-svn-id: https://svn.mythkast.net/svn/bigpot@48 2707ce61-8f64-3b48-aa73-004342374819

--- BigPot/BigPotConfig.cpp
+++ BigPot/BigPotConfig.cpp
@@ -12,6 +12,7 @@ BigPotConfig::~BigPotConfig()
 
 void BigPotConfig::init()
 {
+#ifdef USINGJSON
 	_filename = filepath_ + "/config.json";
 	printf("try find config file: %s\n", _filename.c_str());
 
@@ -22,28 +23,50 @@ void BigPotConfig::init()
 
 	if (_value["record"].isObject())
 		_record = _value["record"];
-
-	_doc.LoadFile(_filename.c_str());
-
-	//_doc.FirstChildElement("volume");
-	if (_doc.Error())
-		_doc.Clear();
-	XMLDeclaration *pDel = _doc.NewDeclaration("xml version=\"1.0\" encoding=\"UTF-8\"");
-	_doc.NewElement("volume");
+#else
+	_filename = "config.xml";
+	printf("try find config file: %s\n", _filename.c_str());
+	this->doc = new TiXmlDocument(_filename.c_str());
+	if (doc){
+		canread = doc->LoadFile(TIXML_ENCODING_UTF8);
+		if (canread){
+			tablecontent = doc->FirstChildElement("root"); 
+		}
+		else{
+			tablecontent = new TiXmlElement("root");
+		}
+	}
+	else{
+		tablecontent = new TiXmlElement("root");
+	}
+#endif
 	//_doc.
 }
-
+string BigPotConfig::replace(string str, const char *string_to_replace, const char *new_string)
+{
+	//查找第一个匹配的字符串
+	int index = str.find(string_to_replace);
+	// 如果有匹配的字符串
+	while (index != std::string::npos)
+	{
+		// 替换
+		str.replace(index, strlen(string_to_replace), new_string);
+		// 查找下一个匹配的字符串
+		index = str.find(string_to_replace, index + strlen(new_string));
+	}
+	return str;
+}
 void BigPotConfig::write()
 {
-	Json::StyledWriter writer;
+	_filename = "config.xml";
+	TiXmlDocument xmlDoc(_filename.c_str()); // 建立一个XML文件
 
-	_value["record"] = _record;
-	_content = writer.write(_value);
+	TiXmlDeclaration Declaration("1.0", "utf-8", "yes"); // 声明XML的属性
 
-	ofstream ofs;
-	ofs.open(_filename);
-	ofs << _content;
-
+	xmlDoc.InsertEndChild(Declaration); // 写入基本的XML头结构
+	xmlDoc.InsertEndChild(*tablecontent);
+	xmlDoc.Print();
+	xmlDoc.SaveFile();
 	//auto x=_doc.SaveFile(_filename.c_str());
 }
 
--- BigPot/BigPotConfig.h
+++ BigPot/BigPotConfig.h
@@ -2,19 +2,25 @@
 
 #include "BigPotBase.h"
 #include "json/json.h"
-#include "tinyxml2/tinyxml2.h"
+#include "tinyxml.h"
 #include <stdint.h>
-
-
-using namespace tinyxml2;
+#include "ZBase64.h"
+//#define USINGJSON
 
 class BigPotConfig : public BigPotBase
 {
 private:
-	Json::Value _value, _record;
 	string _content;
 	string _filename;
-	XMLDocument _doc;
+#ifdef USINGJSON
+	Json::Value _value, _record;
+#else
+	TiXmlDocument* doc;
+	TiXmlElement* tablecontent;
+	TiXmlElement* element;
+	TiXmlElement* rootElement;
+	bool canread;
+#endif
 
 
 
@@ -25,6 +31,7 @@ public:
 	void init();
 	void write();
 	//以下函数注意默认值
+#ifdef USINGJSON
 	int getInteger(const char * name, int def = 0)
 	{
 		return _value[name].isInt() ? _value[name].asInt() : def;
@@ -56,5 +63,154 @@ public:
 	}
 	void setRecord(int v, const char * name) { _record[name] = v; }
 	void clearRecord() { _record.clear(); }
+#else
+	int getInteger(const char * name, int def = 0)
+	{
+		if (!canread)return def;
+		auto tmpelement = tablecontent->FirstChildElement(name);
+		if (tmpelement){
+			return atoi(tmpelement->FirstChild()->Value());
+		}
+		else
+			return def;
+		//return _value[name].isInt() ? _value[name].asInt() : def;
+	}
+	string getString(const char * name, string def = "")
+	{
+		if (!canread)return def;
+		auto tmpelement = tablecontent->FirstChildElement(name);
+		if (tmpelement){
+			return (tmpelement->FirstChild()->Value());
+		}
+		else
+			return def;
+		//return _value[name].isString() ? _value[name].asString() : def;
+	}
+	double getDouble(const char * name, double def = 0.0)
+	{
+		if (!canread)return def;
+		auto tmpelement = tablecontent->FirstChildElement(name);
+		if (tmpelement){
+			return atof(tmpelement->FirstChild()->Value());
+		}
+		else
+			return def;
+		//return _value[name].isDouble() ? _value[name].asDouble() : def;
+	}
+	bool getBool(bool &v, const char * name)
+	{
+		//not support
+		return false;
+		//return _value[name].isBool() ? _value[name].asBool() : false;
+	}
+
+	void setInteger(int v, const char * name) {
+		char buf[64] = { 0 };
+#if defined(TIXML_SNPRINTF)		
+		TIXML_SNPRINTF(buf, sizeof(buf), "%d", v);
+#else
+		sprintf(buf, "%d", _value);
+#endif
+		if (tablecontent->FirstChildElement(name)){
+			tablecontent->FirstChildElement(name)->FirstChild()->SetValue(buf);
+		}
+		else{
+			auto tmpelement = new TiXmlElement(name);
+			auto titext = new TiXmlText(buf);
+			tmpelement->InsertEndChild(*titext);
+			setdata(*tmpelement);
+		}
+	}
+
+
+	void setString(const string v, const char * name) {
+		if (tablecontent->FirstChildElement(name)){
+			//auto titext = new TiXmlText(buf);
+			tablecontent->FirstChildElement(name)->FirstChild()->SetValue(v.c_str());
+		}
+		else{
+			auto tmpelement = new TiXmlElement(name);
+			auto titext = new TiXmlText(v.c_str());
+			tmpelement->InsertEndChild(*titext);
+			//tmpelement->SetAttribute(name, v.c_str());
+			setdata(*tmpelement);
+		}
+	}
+
+
+	void setDouble(double v, const char * name) {
+		char buf[64] = { 0 };
+#if defined(TIXML_SNPRINTF)		
+		TIXML_SNPRINTF(buf, sizeof(buf), "%f", v);
+#else
+		sprintf(buf, "%d", _value);
+#endif
+		if (tablecontent->FirstChildElement(name)){
+			tablecontent->FirstChildElement(name)->SetValue(buf);
+		}
+		else{
+			auto tmpelement = new TiXmlElement(name);
+			auto titext = new TiXmlText(buf);
+			tmpelement->InsertEndChild(*titext);
+			setdata(*tmpelement);
+		}
+	}
+	void setdata(TiXmlElement& element){
+		tablecontent->InsertEndChild(element);
+	}
+	void setBool(bool v, const char * name) {
+		//not support
+		//(name, v);
+	}
+	int getRecord(const char * name)
+	{
+		if (!canread)return 0;
+		auto _record = tablecontent->FirstChildElement("record");
+		if (!_record)
+		{
+
+			return 0;
+		}
+		auto tmp = _record->FirstChildElement(ZBase64::Encode((const unsigned char*) name, strlen(name)).c_str());
+		if (tmp){
+			return atoi(tmp->GetText());
+		}
+		else
+			return 0;
+	}
+	void setRecord(int v, const char * name) {
+		char buf[64] = { 0 };
+#if defined(TIXML_SNPRINTF)		
+		TIXML_SNPRINTF(buf, sizeof(buf), "%d", v);
+#else
+		sprintf(buf, "%d", _value);
+#endif
+		auto _record = tablecontent->FirstChildElement("record");
+		if (!_record)
+		{
+			auto titext = new TiXmlText(buf);
+			_record = new TiXmlElement("record");
+			auto newti = new TiXmlElement(ZBase64::Encode((const unsigned char*)name,strlen(name)).c_str());
+			//auto titext = new TiXmlText(buf);
+			newti->InsertEndChild(*titext);
+			_record->InsertEndChild(*newti);
+			setdata(*_record);
+		}
+		else{
+			_record->FirstChildElement(ZBase64::Encode((const unsigned char*) name, strlen(name)).c_str())->FirstChild()->SetValue(buf);
+			//auto newti = new TiXmlElement(ZBase64::Encode((const unsigned char*) name, strlen(name)).c_str());
+			//newti->InsertEndChild(*titext);
+			//_record->InsertEndChild(*newti);
+		}
+	}
+	void clearRecord() {
+		if (!canread)return;
+		auto _record = tablecontent->FirstChildElement("record");
+		if (_record){
+			_record->Clear();
+		}
+	}
+	string replace(string str, const char *string_to_replace, const char *new_string);
+#endif
 };
 
