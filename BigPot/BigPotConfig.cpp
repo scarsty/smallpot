a9b24fb0ff84fcf4aaf2f50b39d30e3d0cd71b95
cd <cd@2707ce61-8f64-3b48-aa73-004342374819>
2015-03-15 16:35:28 +0000

    aa
    git-svn-id: https://svn.mythkast.net/svn/bigpot@49 2707ce61-8f64-3b48-aa73-004342374819

--- BigPot/BigPotConfig.cpp
+++ BigPot/BigPotConfig.cpp
@@ -3,11 +3,13 @@
 
 BigPotConfig::BigPotConfig()
 {
+	//doc = new XMLDocument;
 }
 
 
 BigPotConfig::~BigPotConfig()
 {
+	//delete doc;
 }
 
 void BigPotConfig::init()
@@ -15,7 +17,6 @@ void BigPotConfig::init()
 #ifdef USINGJSON
 	_filename = filepath_ + "/config.json";
 	printf("try find config file: %s\n", _filename.c_str());
-
 	
 	Json::Reader reader;
 	_content = readStringFromFile(_filename);
@@ -24,49 +25,35 @@ void BigPotConfig::init()
 	if (_value["record"].isObject())
 		_record = _value["record"];
 #else
-	_filename = "config.xml";
+	_filename = filepath_ + "/config.xml";
 	printf("try find config file: %s\n", _filename.c_str());
-	this->doc = new TiXmlDocument(_filename.c_str());
-	if (doc){
-		canread = doc->LoadFile(TIXML_ENCODING_UTF8);
-		if (canread){
-			tablecontent = doc->FirstChildElement("root"); 
-		}
-		else{
-			tablecontent = new TiXmlElement("root");
-		}
-	}
-	else{
-		tablecontent = new TiXmlElement("root");
-	}
+	_doc.LoadFile(_filename.c_str());
+#ifdef _DEBUG
+	_doc.Print();
 #endif
-	//_doc.
-}
-string BigPotConfig::replace(string str, const char *string_to_replace, const char *new_string)
-{
-	//查找第一个匹配的字符串
-	int index = str.find(string_to_replace);
-	// 如果有匹配的字符串
-	while (index != std::string::npos)
+	//初始化结构
+	if (_doc.Error())
 	{
-		// 替换
-		str.replace(index, strlen(string_to_replace), new_string);
-		// 查找下一个匹配的字符串
-		index = str.find(string_to_replace, index + strlen(new_string));
+		_root = _doc.NewElement("root");
 	}
-	return str;
+	else
+	{
+		_root = _doc.FirstChildElement("root");
+	}
+
+	_record = _root->FirstChildElement("record");
+	if (!_record)
+		_record = _root->InsertFirstChild(_doc.NewElement("record"))->ToElement();
+#endif
 }
+
 void BigPotConfig::write()
 {
-	_filename = "config.xml";
-	TiXmlDocument xmlDoc(_filename.c_str()); // 建立一个XML文件
-
-	TiXmlDeclaration Declaration("1.0", "utf-8", "yes"); // 声明XML的属性
-
-	xmlDoc.InsertEndChild(Declaration); // 写入基本的XML头结构
-	xmlDoc.InsertEndChild(*tablecontent);
-	xmlDoc.Print();
-	xmlDoc.SaveFile();
-	//auto x=_doc.SaveFile(_filename.c_str());
+#ifdef USINGJSON
+#else
+	_doc.LinkEndChild(_doc.NewDeclaration());
+	_doc.LinkEndChild(_root);
+	_doc.SaveFile(_filename.c_str());
+#endif
 }
 
