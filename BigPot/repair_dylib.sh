4696303a46f47e14d813abaea080c48eeb9313e1
scarsty <scarsty@gmail.com>
2015-03-24 14:13:37 +0800

    完善mac版

--- BigPot/repair_dylib.sh
+++ BigPot/repair_dylib.sh
@@ -5,15 +5,17 @@
 
 BIN=$1
 BINNAME=`basename $BIN`
-DYLIBS=`otool -L $BIN | grep "$2" | awk -F' ' '{ print $1 }'`
+DYLIBS=`otool -L $BIN | grep "/usr/local" | awk -F' ' '{ print $1 }'`
 
-#for dylib in $DYLIBS; do		
-#	echo found $dylib
-#done
+for dylib in $DYLIBS; do		
+	echo found $dylib
+	cp $dylib ../lib
+done
 
 for dylib in $DYLIBS; do
 	#echo "s/$2/$3/g"
-	new=`echo $dylib | sed s/$2/$3/g`
+	#new=`echo $dylib | sed s/$2/$3/g`
+	new=@loader_path/../lib/`basename $dylib`
 	echo try change $dylib to $new
 	if [ "$BINNAME" != "$name" ]; then
 		install_name_tool -change $dylib $new $BIN
