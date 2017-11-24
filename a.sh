3f57eb1920cf9910306be90b9eede9150e86fcc4
scarsty <scarsty@gmail.com>
2017-11-24 12:29:54 +0800

    更改动态库的脚本

--- a.sh
+++ a.sh
@@ -1,8 +1,9 @@
 cd src
 rm tinypot
 make -f Makefile.clang 
-./repair_dylib.sh tinypot loader_path loader_path\\\/\\\.\\\.\\\/lib
-./repair_dylib.sh tinypot rpath loader_path\\\/\\\.\\\.\\\/lib
 otool -L tinypot
 cd ..
 cp src/tinypot tinypot.app/Contents/MacOS/tinypot
+cd tinypot.app/Contents/MacOS
+../../../src/repair_dylib.sh
+otool -L tinypot
