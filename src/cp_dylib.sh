3024e3e275364a585e502ad1161351e6d5d4584f
scarsty <scarsty@gmail.com>
2018-06-06 13:36:08 +0800

    no message

--- src/cp_dylib.sh
+++ src/cp_dylib.sh
@@ -6,9 +6,13 @@ for dylib in $DYLIBS; do
 	echo found existing file $dylib
 	BIN=$dylib
 	DYLIBS2=`otool -L $BIN | grep "/usr/local" | grep $BIN -v | awk -F' ' '{ print $1 }'`
-	for dylib2 in $DYLIBS2; do		
+	for dylib2 in $DYLIBS2; do
 		echo ' ' found relied file $dylib2
-		cp -p $dylib2 ../lib
+		basename2=`basename $dylib2`
+		if [ ! -f "$basename2" ]; then 
+		    echo "  cp dylib"
+			cp $dylib2 ../lib 
+	    fi
 	done
 done
 
